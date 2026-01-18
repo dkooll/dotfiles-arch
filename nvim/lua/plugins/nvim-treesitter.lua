645fe87be32f3b7edb25565b36d9c226bb630be2
dkooll <dennis.kool@cloudnation.nl>
2026-01-18 13:57:26 +0100

    fix treesitter issues

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -44,48 +44,40 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      local select = require("nvim-treesitter-textobjects.select")
-      local move = require("nvim-treesitter-textobjects.move")
+      local TS = require("nvim-treesitter-textobjects")
+      if TS.setup then
+        TS.setup({
+          move = {
+            enable = true,
+            set_jumps = true,
+          },
+        })
+      end
 
-      -- Select keymaps
-      vim.keymap.set({ "x", "o" }, "af", function()
-        select.select_textobject("@function.outer", "textobjects")
-      end, { desc = "Select outer function" })
-      vim.keymap.set({ "x", "o" }, "if", function()
-        select.select_textobject("@function.inner", "textobjects")
-      end, { desc = "Select inner function" })
-      vim.keymap.set({ "x", "o" }, "ac", function()
-        select.select_textobject("@class.outer", "textobjects")
-      end, { desc = "Select outer class" })
-      vim.keymap.set({ "x", "o" }, "ic", function()
-        select.select_textobject("@class.inner", "textobjects")
-      end, { desc = "Select inner class" })
+      -- Set up keymaps via autocmd to ensure plugin is loaded
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          local buf = ev.buf
+          local move = require("nvim-treesitter-textobjects.move")
 
-      -- Move keymaps
-      vim.keymap.set({ "n", "x", "o" }, "]m", function()
-        move.goto_next_start("@function.outer", "textobjects")
-      end, { desc = "Next function start" })
-      vim.keymap.set({ "n", "x", "o" }, "]M", function()
-        move.goto_next_end("@function.outer", "textobjects")
-      end, { desc = "Next function end" })
-      vim.keymap.set({ "n", "x", "o" }, "[m", function()
-        move.goto_previous_start("@function.outer", "textobjects")
-      end, { desc = "Previous function start" })
-      vim.keymap.set({ "n", "x", "o" }, "[M", function()
-        move.goto_previous_end("@function.outer", "textobjects")
-      end, { desc = "Previous function end" })
-      vim.keymap.set({ "n", "x", "o" }, "]]", function()
-        move.goto_next_start("@class.outer", "textobjects")
-      end, { desc = "Next class start" })
-      vim.keymap.set({ "n", "x", "o" }, "][", function()
-        move.goto_next_end("@class.outer", "textobjects")
-      end, { desc = "Next class end" })
-      vim.keymap.set({ "n", "x", "o" }, "[[", function()
-        move.goto_previous_start("@class.outer", "textobjects")
-      end, { desc = "Previous class start" })
-      vim.keymap.set({ "n", "x", "o" }, "[]", function()
-        move.goto_previous_end("@class.outer", "textobjects")
-      end, { desc = "Previous class end" })
+          local keymaps = {
+            { "]m", "goto_next_start", "@function.outer" },
+            { "]M", "goto_next_end", "@function.outer" },
+            { "[m", "goto_previous_start", "@function.outer" },
+            { "[M", "goto_previous_end", "@function.outer" },
+            { "]]", "goto_next_start", "@class.outer" },
+            { "][", "goto_next_end", "@class.outer" },
+            { "[[", "goto_previous_start", "@class.outer" },
+            { "[]", "goto_previous_end", "@class.outer" },
+          }
+
+          for _, map in ipairs(keymaps) do
+            vim.keymap.set({ "n", "x", "o" }, map[1], function()
+              move[map[2]](map[3], "textobjects")
+            end, { buffer = buf, silent = true })
+          end
+        end,
+      })
     end,
   },
   {
