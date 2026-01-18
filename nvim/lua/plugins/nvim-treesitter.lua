adb50552b10a2d358ba1939cdcccb9163f268c64
dkooll <dennis.kool@cloudnation.nl>
2026-01-18 16:00:39 +0100

    fix rust analyzer issues

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,98 +1,108 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = function()
-    local ok, install = pcall(require, "nvim-treesitter.install")
-    if not ok then
-      return
-    end
-    install.prefer_git = true
-    local orig_print = _G.print
-    _G.print = function() end
-    pcall(install.update, { with_sync = true })
-    _G.print = orig_print
-  end,
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  opts = {
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = function()
+      local ok, install = pcall(require, "nvim-treesitter.install")
+      if not ok then
+        return
+      end
+      install.prefer_git = true
+      local orig_print = _G.print
+      _G.print = function() end
+      pcall(install.update, { with_sync = true })
+      _G.print = orig_print
+    end,
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    indent = { enable = true },
-    autotag = { enable = true },
-    ensure_installed = {
-      "bash",
-      "go",
-      "gomod",
-      "gosum",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "regex",
-      "rust",
-      "terraform",
-      "hcl",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    modules = {},
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<leader>vv",
-        node_incremental = "<leader>vv",
-        scope_incremental = false,
-        node_decremental = "<BS>",
-      },
-    },
-    textobjects = {
-      select = {
+    opts = {
+      highlight = {
         enable = true,
-        lookahead = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      autotag = { enable = true },
+      ensure_installed = {
+        "bash",
+        "go",
+        "gomod",
+        "gosum",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "terraform",
+        "hcl",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
+      modules = {},
+      incremental_selection = {
+        enable = true,
         keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          init_selection = "<leader>vv",
+          node_incremental = "<leader>vv",
+          scope_incremental = false,
+          node_decremental = "<BS>",
         },
       },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
         },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
         },
       },
     },
+    config = function(_, opts)
+      local ok, configs = pcall(require, "nvim-treesitter.configs")
+      if not ok then
+        vim.notify("nvim-treesitter not available; skipping setup", vim.log.levels.WARN)
+        return
+      end
+      configs.setup(opts)
+    end,
   },
-  config = function(_, opts)
-    local ok, configs = pcall(require, "nvim-treesitter.configs")
-    if not ok then
-      vim.notify("nvim-treesitter not available; skipping setup", vim.log.levels.WARN)
-      return
-    end
-    configs.setup(opts)
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      pcall(require, "nvim-treesitter.configs")
+    end,
+  },
 }
